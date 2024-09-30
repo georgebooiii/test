138b1a1042d596adfb89c64a9cc812ca3eb7673c
georgii <georgiy.losev.201@mail.ru>
2024-09-30 21:41:00 +0300

    finished

---
+++ mymod.py
@@ -0,0 +1,10 @@
+def hello():
+    print("hello")
+    def mysum(a,b):
+        return a+b
+    def mysub(a,b):
+        return a-b
+    def mymul(a,b):
+        return a*b:
+    def mydiv(a,b):
+        return a/b
